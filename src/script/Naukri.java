ac618e973f32ef92bba08f10c56a224ef9be6c73
Administrator <Administrator@Android555>
2018-10-04 16:57:48 +0530

    DATA DRIVEN FRAMEWORK

--- src/script/Naukri.java
+++ src/script/Naukri.java
@@ -19,10 +19,12 @@ public class Naukri implements Auto{
 			 String em=Excel.abc(excelpath, "sheet1", 0, 1);
 			 String pwd=Excel.abc(excelpath, "sheet1", 0, 2);
 			 
-			 driver.get("https://www.naukri.com/nlogin/login?msg=0&URL=http%3A%2F%2Fmy.naukri.com");
+			    driver.get("https://www.naukri.com/nlogin/login?msg=0&URL=http%3A%2F%2Fmy.naukri.com");
 				driver.findElement(By.id("usernameField")).sendKeys(em);
 				driver.findElement(By.id("passwordField")).sendKeys(pwd);
 				driver.findElement(By.xpath("//button[text()='Login']")).click();
+				
+				driver.close();
 		}
 
 	}
